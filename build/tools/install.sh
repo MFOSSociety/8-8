e0b085a513974a354f401c6cb82ff9ef83b7c745
Piyush Raj <piyush.189303055@muj.manipal.edu>
2021-07-25 18:03:42 +0530

    minor: refined .gitignore

---
+++ build/tools/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get install libffi-dev libbz2-dev liblzma-dev libsqlite3-dev libncurses5-dev libgdbm-dev zlib1g-dev libreadline-dev libssl-dev tk-dev build-essential libncursesw5-dev libc6-dev openssl git python3-rpi.gpio
